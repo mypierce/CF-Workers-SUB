d1cca348274f1826112b911ee964ee5774f00ae0
mypierce <myanata@gmail.com>
2024-10-21 16:40:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ https://sub.xf.free.hr/auto
 
 let urls = [];
 let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://github.com/mypierce/ACL4SSR/blob/master/Clash/config/ACL4SSR_Online_Mini_MultiCountry.ini"; //订阅配置文件
+let subconfig = "https://github.com/mypierce/ACL4SSR/blob/master/Clash/config/ACL4SSR_Online_Full.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
