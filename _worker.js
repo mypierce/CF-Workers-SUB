8b1ecaebaea29bf24cc348ef9ac9956f642eecbb
mypierce <myanata@gmail.com>
2026-07-15 15:51:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ https://cfxr.eu.org/getSub
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://github.com/mypierce/OpenClash-Templates/blob/main/fzlwhyc-openclash.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/mypierce/OpenClash-Templates/refs/heads/main/fzlwhyc-openclash.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
